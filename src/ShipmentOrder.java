c1106e8c2cd2ee971d76ce03e571c8154a2cf4cd
Herman <s32370@pjwstk.edu.pl>
2026-05-05 13:00:34 +0200

    geters setters, shipment order

--- src/ShipmentOrder.java
+++ src/ShipmentOrder.java
@@ -1,17 +1,40 @@
 public abstract class ShipmentOrder implements  SummaryPrintable{
 
     //2. Abstract Class
-    //Create exactly one abstract class named ShipmentOrder. This class should implement SummaryPrintable.
-    //
-    //The class should store common data:
-    //
-    //orderNumber - unique shipment order number,
-    //customerName - customer name,
-    //distanceKm - delivery distance in kilometers,
-    //baseFee - base shipment fee,
-    //insured - information whether the shipment is insured,
-    //lastCalculatedPrice - last price calculated during processing.
+
+    private String orderNumber; // - unique shipment order number,
+    private String customerName; // - customer name,
+    private doubel distanceKm; // - delivery distance in kilometers,
+    private double baseFee; // - base shipment fee,
+    private boolean insured; // - information whether the shipment is insured,
+    private double lastCalculatedPrice; // - last price calculated during processing.
+
     //Prepare a constructor and getters needed by subclasses. The class should also contain the template method:
+
+    public ShipmentOrder(String orderNumber, String customerName, double distanceKm,
+                         double baseFee, boolean insured){
+        this.orderNumber=orderNumber;
+        this.customerName=customerName;
+        this.distanceKm=distanceKm;
+        this.baseFee=baseFee;
+        this.insured=insured;
+    }
+
+    protected String getOrderNumber(){
+        return orderNumber;
+    }
+    protected String getCustomerName(){
+        return customerName;
+    }
+    protected double getDistanceKm(){
+        return distanceKm;
+    }
+    protected double getBaseFee(){
+        return baseFee;
+    }
+    protected boolean isInsured(){
+        return insured;
+    }
 
     public final void processOrder() {
         validateOrder();
@@ -25,10 +48,13 @@ public abstract class ShipmentOrder implements  SummaryPrintable{
         lastCalculatedPrice = price;
         printProcessingResult();
     }
-//Template Method requirement. The method processOrder() must be implemented only in ShipmentOrder. Subclasses should not override it.
+//Template Method requirement. The method  must be implemented only in ShipmentOrder. Subclasses should not override it.
 //
 //The abstract class should contain these common methods:
-//
+
+
+    processOrder()
+
 //private void validateOrder() - checks common fields, for example empty order number or non-positive distance,
 //protected void validateSpecificRules() - a hook method with an empty default implementation,
 //private double applyInsurance(double price) - if insured is true, adds 7% to the current price,
